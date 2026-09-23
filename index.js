9f9f31232b6665508a14c0143620b720230d3cad
MrPapuNoob <yostinbrito@hotmail.com>
2026-09-22 20:21:44 -0400

    fix minimo 2

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+import "dotenv/config"
 import express from "express"
 import librosRoutes from "./src/routes/libros.routes.js"
 import authRoutes from "./src/routes/authRoute.js"
 
 const app = express()
 app.use(express.json())
-app.use("/registro", authRoutes)
-app.use("/login", authRoutes)
+app.use("/auth/registro", authRoutes)
+app.use("/auth/login", authRoutes)
 app.use("/libros", librosRoutes)
 
-app.listen(3000, () => {
-  console.log("Servidor escuchando en el puerto 3000")
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Servidor escuchando en el puerto " + (process.env.PORT || 3000))
 })
--- src/routes/authRoute.js
+++ src/routes/authRoute.js
@@ -3,7 +3,7 @@ import { registro, login } from "../controllers/auth.controller.js"
 
 const router = Router()
 
-router.post("/registro", registro)
-router.post("/login", login)
+router.post("/auth/registro", registro)
+router.post("/auth/login", login)
 
 export default router
